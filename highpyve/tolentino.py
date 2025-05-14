2efb52fb3f63ddec24ca5c6cb208ec8e02da3d42
Tolentino-MaRose <maroseltolentino@iskolarngbayan.pup.edu.ph>
2025-05-14 23:44:42 +0800

    add clear screen in main.py

--- highpyve/tolentino.py
+++ highpyve/tolentino.py
@@ -3,5 +3,5 @@ import getname
 def get_cat_name(owner_name):
     """Generates a random cat name and prints it with the owner's name."""
     cat_name = getname.random_name('cat')
-    print(f"\n{owner_name}, your cat's name is {cat_name}.")
+    print(f"\n{owner_name}, your cat's name is {cat_name}.\n")
     
--- main.py
+++ main.py
@@ -1,32 +1,41 @@
+import os
 from highpyve import tolentino, ocariza, anipan, bayos, bartolome
 
 def pause():
     input("Press Enter to continue...")
+    
+def clear():
+    os.system('cls')
 
+clear()
 print("Prints a personalized cat name based on the user's name")
 print("=" * 50)
-user_name = input("Enter your name to generate your cat's name: ")
+user_name = input("\nEnter your name to generate your cat's name: ")
 capitalized_username = user_name.title()
 tolentino.get_cat_name(capitalized_username)
 print("=" * 50 + "\n")
 pause()
+clear()
 
 print("\nGenerates a birthday summary showing how old the user is")
 print(ocariza.birthday_summary())
 print("=" * 50 + "\n")
 pause()
+clear()
 
 print("\nCreates a randomly generated fake person profile")
 print("=" * 50)
 anipan.create_fake_profile()
 print("=" * 50 + "\n")
 pause()
+clear()
 
 print("\nCreates a fake animal profile")
 print("=" * 50)
 bayos.create_fake_animal_profile()
 print("=" * 50 + "\n")
 pause()
+clear()
 
 print("\nPrints random jokes for fun and laughter")
 print("=" * 50)
