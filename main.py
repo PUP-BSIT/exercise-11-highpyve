a707f705ddaf1e5d125c35c5a813f7a54dd1c540
Tolentino-MaRose <maroseltolentino@iskolarngbayan.pup.edu.ph>
2025-05-14 23:20:08 +0800

    update main.py

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ from highpyve import tolentino, ocariza, anipan, bayos, bartolome
 
 print("Rose Tolentino")
 print("=" * 50)
-tolentino.get_cat_name("Rose")
+user_name = input("Enter your name to generate your cat's name: ")
+capitalize_username = user_name.title()
+tolentino.get_cat_name(capitalize_username)
 print("=" * 50 + "\n")
 
 print("Jaira Ocariza")
