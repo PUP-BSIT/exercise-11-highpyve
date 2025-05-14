64fa0ecb55b0b05874aaa987a322d4463e0ff682
Anipan_SigmundKristoffer <krisanipan2205@gmail.com>
2025-05-14 18:05:10 +0800

    Make the code more readable

--- main.py
+++ main.py
@@ -1,10 +1,23 @@
 from highpyve import tolentino, ocariza, anipan, bayos, bartolome
 
+
+print("Rose Tolentino")
+print("=" * 50)
 tolentino.get_cat_name("Rose")
-print()
+print("=" * 50 + "\n")
+
+print("Jaira Ocariza")
 print(ocariza.birthday_summary())
-print()
+print("=" * 50 + "\n")
+
+print("Kristoffer Anipan")
 anipan.create_fake_profile()
-print()
+print("=" * 50 + "\n")
+
+print("Eurielle Bayos")
 bayos.create_fake_animal_profile()
+print("=" * 50 + "\n")
+
+print("Bartolome's Jokes")
 bartolome.get_jokes()
+print("=" * 50 + "\n")
