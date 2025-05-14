d96341f9537b34e904eeb3c88c5c9672a10e8a9a
Mikaela Joy Bartolome <mikaela.bartolome13@gmail.com>
2025-05-14 17:47:51 +0800

    add bartolome's module to main.py

--- main.py
+++ main.py
@@ -1,18 +1,10 @@
-from highpyve import tolentino, ocariza, anipan, bayos
+from highpyve import tolentino, ocariza, anipan, bayos, bartolome
 
-# Import member's module
-# TODO (BARTOLOME): Import module
-
-
-
-# Add function calls
 tolentino.get_cat_name("Rose")
 print()
 print(ocariza.birthday_summary())
 print()
 anipan.create_fake_profile()
-# TODO (BARTOLOME): Call main function
 print()
 bayos.create_fake_animal_profile()
-
-
+bartolome.get_jokes()
