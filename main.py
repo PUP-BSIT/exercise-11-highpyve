3c0cc6a063bf44d4c0644dd6dae6430215ccf917
Ocariza_JairaIsabel <jairaocariza@gmail.com>
2025-05-14 16:59:31 +0800

    add main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Import member's module
+# TODO (ANIPAN): Import module
+# TODO (BARTOLOME): Import module
+# TODO (BAYOS): Import module
+# TODO (OCARIZA): Import module
+# TODO (TOLENTINO): Import module
+
+# Add function calls
+# TODO (ANIPAN): Call main function
+# TODO (BARTOLOME): Call main function
+# TODO (BAYOS): Call main function
+# TODO (OCARIZA): Call main function
+# TODO (TOLENTINO): Call main function
