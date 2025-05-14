ab262c6480573e8581a4bbe7d7fbbf009be1b477
Tolentino-MaRose <maroseltolentino@iskolarngbayan.pup.edu.ph>
2025-05-14 17:08:39 +0800

    add tolentino's module to main.py

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
+from highpyve import tolentino
+
 # Import member's module
 # TODO (ANIPAN): Import module
 # TODO (BARTOLOME): Import module
 # TODO (BAYOS): Import module
 # TODO (OCARIZA): Import module
-# TODO (TOLENTINO): Import module
+
 
 # Add function calls
+tolentino.get_cat_name("Rose")
 # TODO (ANIPAN): Call main function
 # TODO (BARTOLOME): Call main function
 # TODO (BAYOS): Call main function
 # TODO (OCARIZA): Call main function
-# TODO (TOLENTINO): Call main function
+
